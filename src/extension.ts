6c6c1aa028dffdd62c36df6aba3c23fb83eda550
Max Fend <maxgfend@gmail.com>
2026-03-26 20:13:47 +0100

    Initial Extension Code

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,16 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
     console.log('LocKick is now active.');
 
-    let disposable = vscode.commands.registerCommand('lockick.testConnection', () => {
-        const config = vscode.workspace.getConfiguration('lockick');
-        const url = config.get<string>('serverUrl');
-        vscode.window.showInformationMessage(`Testing connection to: ${url}`);
-    });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(
+        vscode.commands.registerCommand('lockick.testConnection', () => {
+            const config = vscode.workspace.getConfiguration('lockick');
+            const url = config.get<string>('serverUrl');
+            vscode.window.showInformationMessage(`LocKick: Testing connection to: ${url}`);
+        }),
+        vscode.commands.registerCommand('lockick.askAboutSelection', () => {
+            vscode.window.showInformationMessage('LocKick: Asking about selection...');
+        })
+    );
 }
 
 export function deactivate() {}
